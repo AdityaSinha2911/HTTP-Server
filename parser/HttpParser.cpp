e9fe4fee5f5fdc30ba9122ea4c029b2c4e6a27e7
Aditya <adityahjp11111@gmail.com>
2026-08-01 01:25:43 +0530

    feat: parse request line implemented

--- parser/HttpParser.cpp
+++ parser/HttpParser.cpp
@@ -1,10 +1,29 @@
 #include "HttpParser.h"
+#include <sstream>
 
 HttpRequest HttpParser::parse(const std::string& rawRequest)
 {
     HttpRequest request;
 
-    // Parsing logic will be added 
+    // Create a stream from the raw HTTP request
+    std::istringstream requestStream(rawRequest);
+
+    // Store the first line (Request Line)
+    std::string requestLine;
+
+    // Read the first line
+    std::getline(requestStream, requestLine);
+
+    // Remove '\r' if present
+    if (!requestLine.empty() && requestLine.back() == '\r'){
+        requestLine.pop_back();
+    }
+
+    // Create another stream for the request line
+    std::istringstream lineStream(requestLine);
+
+    // Extract Method, Path and Version
+    lineStream >> request.method>> request.path>> request.version;
 
     return request;
 }
