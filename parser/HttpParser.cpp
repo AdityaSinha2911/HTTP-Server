e652decba68b04aa03e73bbac5934c4dfd3a442c
Aditya <adityahjp11111@gmail.com>
2026-08-01 00:59:15 +0530

    Http Parser intial file added

---
+++ parser/HttpParser.cpp
@@ -0,0 +1,10 @@
+#include "HttpParser.h"
+
+HttpRequest HttpParser::parse(const std::string& rawRequest)
+{
+    HttpRequest request;
+
+    // Parsing logic will be added 
+
+    return request;
+}
