47800c2b3d38668088a492751eda16cc9b236cfd
Aditya <adityahjp11111@gmail.com>
2026-08-01 02:00:51 +0530

    feat: parse header implemented

--- parser/HttpParser.cpp
+++ parser/HttpParser.cpp
@@ -9,21 +9,53 @@ HttpRequest HttpParser::parse(const std::string& rawRequest)
     std::istringstream requestStream(rawRequest);
 
     // Store the first line (Request Line)
-    std::string requestLine;
+    std::string line;
 
     // Read the first line
-    std::getline(requestStream, requestLine);
+    std::getline(requestStream, line);
 
     // Remove '\r' if present
-    if (!requestLine.empty() && requestLine.back() == '\r'){
-        requestLine.pop_back();
+    if (!line.empty() && line.back() == '\r'){
+        line.pop_back();
     }
 
     // Create another stream for the request line
-    std::istringstream lineStream(requestLine);
+    std::istringstream lineStream(line);
 
     // Extract Method, Path and Version
     lineStream >> request.method>> request.path>> request.version;
+
+
+    // Parse Headers
+    
+    while (std::getline(requestStream, line))
+    {
+        // Remove '\r'
+        if (!line.empty() && line.back() == '\r')
+        {
+            line.pop_back();
+        }
+
+        // Blank line = End of Headers
+        if (line.empty())
+        {
+            break;
+        }
+
+        // Find ':'
+        size_t colonPos = line.find(':');
+
+        if (colonPos == std::string::npos)
+        {
+            continue;
+        }
+
+        std::string key = line.substr(0, colonPos);
+
+        std::string value = line.substr(colonPos + 2);
+
+        request.headers[key] = value;
+    }
 
     return request;
 }
