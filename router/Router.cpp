f22042b363132a19ea22cadfe9c121f1ab190251
Aditya <adityahjp11111@gmail.com>
2026-08-13 07:30:06 +0530

    integrated file reader, to read .html files

--- router/Router.cpp
+++ router/Router.cpp
@@ -1,45 +1,45 @@
 #include "Router.h"
+#include "../file/FileReader.h"
 
 HttpResponse Router::route(const HttpRequest& request)
 {
     HttpResponse response;
 
+    FileReader reader;
+
+    // Decide which file should be served
+    std::string filePath;
+
     if (request.path == "/")
     {
-        response.statusCode = 200;
-        response.statusMessage = "OK";
-
-        response.headers["Content-Type"] = "text/html";
-
-        response.body = "<h1>Home Page</h1>";
-    }
-    else if (request.path == "/about")
-    {
-        response.statusCode = 200;
-        response.statusMessage = "OK";
-
-        response.headers["Content-Type"] = "text/html";
-
-        response.body ="<h1>About Page</h1>";
-    }
-    else if (request.path == "/contact")
-    {
-        response.statusCode = 200;
-        response.statusMessage = "OK";
-
-        response.headers["Content-Type"] = "text/html";
-
-        response.body ="<h1>Contact Page</h1>";
+        filePath = "public/index.html";
     }
     else
     {
+        filePath = "public" + request.path;
+    }
+
+    // Read requested file
+    std::string content = reader.read(filePath);
+
+    // File could not be read
+    if (content.empty())
+    {
         response.statusCode = 404;
         response.statusMessage = "Not Found";
-
         response.headers["Content-Type"] = "text/html";
 
-        response.body ="<h1>404 Not Found</h1>";
+        response.body = "<h1>404 Not Found</h1>";
+
+        return response;
     }
+
+    // File successfully read
+    response.statusCode = 200;
+    response.statusMessage = "OK";
+    response.headers["Content-Type"] = "text/html";
+
+    response.body = content;
 
     return response;
 }
